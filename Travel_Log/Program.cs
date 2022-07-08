2a2d8dae0f16c7031d01ae4c728ca1a2d791bdeb
David York <davidyork1995@gmail.com>
2022-07-08 11:11:34 -0400

    Wrote intitial greeting messages.

---
+++ Travel_Log/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace The_Daily_Travel_Log
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Title = "Travel Log";
+            Console.Clear();
+            Console.WriteLine("Welcome to The Daily Travel Log!");
+            Console.WriteLine();
+            Thread.Sleep(2000);
+            Console.WriteLine("Please select an option:");
+        }
+    }
+}
