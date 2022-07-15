de9f7454bba864e2e9d7d32e2a10b5db579ab32a
David York <davidyork1995@gmail.com>
2022-07-15 15:15:51 -0400

    Shorted sleep times and added more accepted inputs.

--- Travel_Log/MainMenu.cs
+++ Travel_Log/MainMenu.cs
@@ -9,33 +9,33 @@ namespace The_Daily_Travel_Log
         public static void Menu()
         {
             Console.WriteLine("Please type the corresponding number to select an option:");
-            Thread.Sleep(1000);
+            Thread.Sleep(500);
             Console.WriteLine("1: Log Location");
-            Thread.Sleep(1000);
+            Thread.Sleep(500);
             Console.WriteLine("2: See Previous Locations");
-            Thread.Sleep(1000);
+            Thread.Sleep(500);
             Console.WriteLine("3: Return to Menu");
-            Thread.Sleep(1000);
+            Thread.Sleep(500);
             Console.WriteLine("4: Quit");
 
             Console.WriteLine();
             string menuoption = Console.ReadLine();
-            if (menuoption == "1" || menuoption == "1." || menuoption == "1:")
+            if (menuoption == "1" || menuoption == "1." || menuoption == "1:" || menuoption == "1)" || menuoption == "1 " || menuoption == "1. " || menuoption == "1: " || menuoption == "1) ")
             {
                 {
                     //this menu option has not implemented yet
                     Console.WriteLine("This feature is still under development!");
-                    Thread.Sleep(1000);
+                    Thread.Sleep(1500);
                     Console.WriteLine();
                     MainMenu.Menu();
                 }
             }
-            else if (menuoption == "2" || menuoption == "2." || menuoption == "2:")
+            else if (menuoption == "2" || menuoption == "2." || menuoption == "2:" || menuoption == "2)" || menuoption == "2 " || menuoption == "2. " || menuoption == "2: " || menuoption == "2) ")
             {
                 { 
                     //this menu option has not been implemented yet
                     Console.WriteLine("This feature is still under development!");
-                    Thread.Sleep(1000);
+                    Thread.Sleep(1500);
                     Console.WriteLine();
                     MainMenu.Menu();
                 }
--- Travel_Log/Program.cs
+++ Travel_Log/Program.cs
@@ -10,6 +10,7 @@ namespace The_Daily_Travel_Log
             Console.Title = "Travel Log";
             Console.Clear();
             Console.WriteLine("Welcome to The Daily Travel Log!");
+            Console.WriteLine();
             Console.WriteLine("The time and date is:");
             Console.WriteLine(DateTime.Now.ToString("HH:mm\nMM/dd/yyyy"));
             Console.WriteLine();
