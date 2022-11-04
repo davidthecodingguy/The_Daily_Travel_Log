adb6143c95b2035d3ec279f549108193b6dee052
David York <davidyork1995@gmail.com>
2022-11-04 14:38:10 -0400

    Updated comment to reflect SOLID

---
+++ Travel_Log/RetrieveData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using NUnit.Compatibility;
+using NUnit.Framework;
+
+namespace The_Daily_Travel_Log
+{
+    //The RetrieveData class has the single responsibility of data retrieval and follows the Single Responsibility Principle (Part of Feature #3)
+    public class RetrieveData
+    {
+        public static void RetrievePreviousLocations()
+        {
+            StreamReader sreader1 = new StreamReader("PreviousLocations.csv");
+            Console.WriteLine(sreader1.ReadToEnd());
+            sreader1.Close();
+        }
+
+        public static void RetrievePreviousErrors()
+        {
+            StreamReader sreader2 = new StreamReader("DailyTravelLogErrors.txt");
+            Console.WriteLine("You are viewing a list of previously logged errors when attempting to access program files or options in the main menu:");
+            Console.WriteLine(sreader2.ReadToEnd());
+            sreader2.Close();
+        }
+    }
+
+}
