5c2d5fb2f0760115d0f01b3dd0cd960a9942b1d4
David York <davidyork1995@gmail.com>
2022-11-09 07:05:20 -0500

    Modified comments to reflect SOLID principles

--- Travel_Log/RetrieveDataTests.cs
+++ Travel_Log/RetrieveDataTests.cs
@@ -7,15 +7,13 @@ using NUnit.Framework;
 namespace The_Daily_Travel_Log
 {
     //The RetrieveDataTests class has the single responsibility of testing data retrieval and follows the Single Responsibility Principle (Part of Feature #3)
-
-    //The EnsureReadCsvFile and EnsureReadTextFile methods can be be extended as new requirements arise without changing the overall function of RetrieveDataTests parent class.
-    //This class follows the Open/Closed Principle (Part of Feature #3)
     [TestFixture]
     public class RetrieveDataTests
     //These unit tests correlate to the unit tests feature listed on the project README (Feature #1) and have been implemented to aid in debugging and ensure the program continues to function as intended
     {
         //TEST TYPE #3: Tests to ensure the program is reading from the correct csv file stored on the computer, like is done in the previous locations display feature (Part of Feature #1)
         [Test]
+        //The EnsureReadCsvFile method's behavior can be extended as new requirements arise without changing it's source code and follows the Open/Closed Principle (Part of Feature #3)
         public void EnsureReadCsvFile()
         {
             RetrieveData.RetrievePreviousLocations();
@@ -23,6 +21,7 @@ namespace The_Daily_Travel_Log
         }
         //Tests to ensure the program is reading from the correct txt file stored on the computer, like is done in the previous errors display feature (Part of Feature #1)
         [Test]
+        //The EnsureReadTxtFile method's behavior can be extended as new requirements arise without changing it's source code and follows the Open/Closed Principle (Part of Feature #3)
         public void EnsureReadTxtFile()
         {
             RetrieveData.RetrievePreviousErrors();
--- Travel_Log/SaveDataTests.cs
+++ Travel_Log/SaveDataTests.cs
@@ -7,9 +7,6 @@ using NUnit.Framework;
 namespace The_Daily_Travel_Log
 {
     //The SaveDataTests class has the single responsibility of testing file/data saving and follows the Single Responsibility Principle (Part of Feature #3)
-
-    //The EnsurePreviousLocationsFileExists, EnsurePreviousErrorsFileExists, EnsureWriteToCsvFile, and EnsureWriteToTxtFile methods can be be extended as new requirements arise without changing the overall function of SaveDataTests parent class.
-    //This class follows the Open/Closed Principle (Part of Feature #3)
     [TestFixture]
     public class SaveDataTests
     //These unit tests correlate to the unit tests feature listed on the project README (Feature #1) and have been implemented to aid in debugging and ensure the program continues to function as intended
@@ -17,6 +14,7 @@ namespace The_Daily_Travel_Log
         //TEST TYPE #1: Tests to ensure the previous locations save file exists without having to run the program or check the computer's memory manually (Part of Feature #1)
         //PLEASE NOTE: Test Type #1 will fail if you have not made an error or attempted to log a location
         [Test]
+        //The EnsurePreviousLocationsFileExists method's behavior can be extended as new requirements arise without changing it's source code and follows the Open/Closed Principle (Part of Feature #3)
         public void EnsurePreviousLocationsFileExists()
         {
             File.Exists("PreviousLocations.csv");
@@ -24,6 +22,7 @@ namespace The_Daily_Travel_Log
         }
         //Tests to ensure the previous errors save file exists without having to run the program or check the computer's memory manually (Part of Feature #1)
         [Test]
+        //The EnsurePreviousErrorsFileExists method's behavior can be extended as new requirements arise without changing it's source code and follows the Open/Closed Principle (Part of Feature #3)
         public void EnsurePreviousErrorsFileExists()
         {
             File.Exists("DailyTravelLogErrors.txt");
@@ -31,6 +30,7 @@ namespace The_Daily_Travel_Log
         }
         //TEST TYPE #2: Tests to ensure the program is writing to a csv file stored on the computer, like is done in the location logging feature, without having to run the program or check the computer's memory manually (Part of Feature #1)
         [Test]
+        //The EnsureWriteToCsvFile method's behavior can be extended as new requirements arise without changing it's source code and follows the Open/Closed Principle (Part of Feature #3)
         public void EnsureWriteToCsvFile()
         {
             SaveData.SaveLocation();
@@ -38,6 +38,7 @@ namespace The_Daily_Travel_Log
         }
         //Tests to ensure the program is writing to a txt file stored on the computer, like is done in the error logging feature, without having to run the program or check the computer's memory manually (Part of Feature #1)
         [Test]
+        //The EnsureWriteToTxtFile method's behavior can be extended as new requirements arise without changing it's source code and follows the Open/Closed Principle (Part of Feature #3)
         public void EnsureWriteToTxtFile()
         {
             ErrorHandling.UserInputError();
