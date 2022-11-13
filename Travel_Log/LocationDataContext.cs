2fb1aef988527bca7d6ff3394d3078d0ec733d93
David York <davidyork1995@gmail.com>
2022-11-13 16:36:53 -0500

    Set up and began creating database model

---
+++ Travel_Log/LocationDataContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.EntityFrameworkCore;
+namespace The_Daily_Travel_Log
+{
+    //I'll probably need to put references to this in SaveData
+    public class LocationDataContext : DbContext
+	{
+		public DbSet<Locations> Locations { get; set; }
+
+		public string DbPath { get; set; }
+
+		public LocationDataContext()
+		{
+			var folder = Environment.SpecialFolder.LocalApplicationData;
+			var path = Environment.GetFolderPath(folder);
+			DbPath = System.IO.Path.Join(path, "locations.db");
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+			options.UseSqlite($"Data Source={DbPath}");
+	}
+    //How do I make sure info goes where I want it? Maybe look at Microsoft notes?
+    public class Locations
+	{
+		public int LocationId { get; set; }
+		public string CurrentTown { get; set; }
+		public string CurrentStateOrProvince { get; set; }
+		public string CurrentCountry { get; set; }
+		public string CurrentDateAndTime { get; set; }
+	}
+}
+//Thanks Doug Sutherland for the database help!
